6cea01cb5a0882d10ab995d7780b9695ccb654db
Tiago Tibes <tiagotibes@gmail.com>
2026-08-14 10:57:03 -0300

    testando correção do servidor externo(supabse/vertex)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-TypeScript
-
+import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 
+const app = express();
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default app;
