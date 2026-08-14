0015f004926981f9250c2d4dc90d809fbaaa3227
Tiago Tibes <tiagotibes@gmail.com>
2026-08-14 13:53:52 -0300

    corrigida a chamdada do swagger no arquvo app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json';
+// Use require to load JSON without needing additional TypeScript config
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerDocument: any = require('./swagger.json');
 
 const app = express();
 
